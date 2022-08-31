17634b5a6758afb4d362ae731bdd26f879777ec1
vasea <vasilii.costiuc@it-lab.md>
2022-08-31 18:35:23 +0300

    layouts

---
+++ resources/views/about.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+@section('content')
+    <!-- Page header with logo and tagline-->
+    <header class="py-5 bg-light border-bottom mb-4">
+        <div class="container">
+            <div class="text-center my-5">
+                <h1 class="fw-bolder">About Us</h1>
+            </div>
+        </div>
+    </header>
+    <!-- Page content-->
+    <div class="container">
+        <div class="row">
+            <!-- Blog entries-->
+            <div class="col-lg-8">
+                <p class="lead mb-0">About us texr</p>
+            </div>
+            <!-- Side widgets-->
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'index')->name('home');
+Route::view('/about', 'about')->name('about');
+Route::view('/contact','contacts')->name('contact');
